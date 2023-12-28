a24f03bf97051b90aae1b3717ae5ec4e4cbca929
Svetlana Novikova <seaciliya2012@yandex.ru>
2023-12-28 12:43:16 +0300

    add_subtraction

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def sum_of_numbers(a: int, b: int):
     sum_nambers = a + b
     return sum_nambers
+
+
+def subtracting_numbers (num_1: int, nam_2: int):
+    new_num = num_1 - nam_2
+    return new_num
